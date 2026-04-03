e2f7aa5cc6dffc88df10ff9eaf9a87732c629b28
SYTan2468 <sheryitan2468@gmail.com>
2026-04-03 22:09:19 +0800

    Update color_sensor_test1.cpp

--- sensor_tests/color_sensor_test1.cpp
+++ sensor_tests/color_sensor_test1.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 
-#define S0_PIN   4
-#define S1_PIN   5
-#define S2_PIN   7
-#define S3_PIN   15  
-#define OUT_PIN  16
-#define LED_PIN  6
+#define S0_PIN   16
+#define S1_PIN   17
+#define S2_PIN   8
+#define S3_PIN   18 
+#define OUT_PIN  3
+#define LED_PIN  15
 
 void setup() {
     Serial.begin(115200);
